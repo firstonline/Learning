7e0bfbb0eb1ef86aa1f9c5a80684079e4da5ba40
Khoa Ngo Quang <KhoaNQ@exient.com>
2020-02-11 10:55:56 +0000

    axe throwing

---
+++ Experimenting/Assets/Scripts/AxeThrowing/Axe.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Axe : MonoBehaviour
+{
+    [SerializeField] private Transform m_parent;
+    [SerializeField] private Transform m_curevePoint;
+    [SerializeField] private Rigidbody m_rigidBody;
+    [SerializeField] private float m_rotationSpeed = 10f;
+    [SerializeField] private float m_returnTime = 2f;
+    [SerializeField] private GameObject m_particlesOnCatch;
+    [SerializeField] private CinemachineImpulseSource m_inpulseSource;
+    [SerializeField] private GameObject m_trail;
+    [SerializeField] private float m_throwRotationRandom = 20f;
+
+    private Vector3 m_landPosition;
+    private Quaternion m_allignmentAngle;
+    private Quaternion m_origRotation;
+    private Vector3 m_originalPosition;
+    private bool m_isReturning;
+    private bool m_flying;
+    private bool m_alligning = false;
+    private float m_currentReturningTime;
+    private Action m_onAxeReturnedCallback;
+
+    private float m_handAdjustmentTime;
+    private float m_handAdjustmentDuration;
+    
+    private void Awake()
+    {
+        m_originalPosition = transform.localPosition;
+        m_origRotation = transform.localRotation;
+
+        m_handAdjustmentTime = 0.9f * m_returnTime;
+        m_handAdjustmentDuration = 0.1f * m_returnTime;
+    }
+
+    private void Update()
+    {
+        if (m_flying)
+        {
+            transform.Rotate(m_rotationSpeed * Time.deltaTime, 0, 0);
+        }
+        else if (m_isReturning)
+        {
+            m_currentReturningTime += Time.deltaTime;
+
+            if (m_currentReturningTime > m_handAdjustmentTime)
+            {
+                if (!m_alligning)
+                {
+                    m_alligning = true;
+                    m_allignmentAngle = transform.localRotation;
+                    m_rigidBody.isKinematic = true;
+                }
+                else
+                {
+                    transform.SetParent(m_parent, true);
+                    float a = m_currentReturningTime - m_handAdjustmentTime;
+                    float b = m_handAdjustmentDuration;
+                    float delta = a / b;
+                    transform.localRotation = Quaternion.Lerp(m_allignmentAngle,
+                        m_origRotation, delta);
+                }
+            }
+            else
+            {
+                transform.Rotate(m_rotationSpeed * Time.deltaTime, 0, 0);
+            }
+            
+            if (m_currentReturningTime >= m_returnTime)
+            {
+                m_currentReturningTime = m_returnTime;
+                m_isReturning = false;
+                transform.SetParent(m_parent, true);
+                transform.localPosition = m_originalPosition;
+                transform.localRotation = m_origRotation;
+                m_onAxeReturnedCallback();
+                m_particlesOnCatch.gameObject.SetActive(true);
+                m_inpulseSource.GenerateImpulse(Vector3.right);
+                m_rigidBody.isKinematic = true;
+                m_trail.gameObject.SetActive(false);
+            }
+            else
+            {
+                transform.position = GetBezierCurve(m_currentReturningTime / m_returnTime,  m_landPosition,m_curevePoint.position, m_parent.position);
+            }
+
+           
+        }
+    }
+
+    public void FlyForward()
+    {
+        m_particlesOnCatch.gameObject.SetActive(false);
+        m_flying = true;
+        m_rigidBody.isKinematic = false;
+        transform.parent = null;
+        transform.position = Camera.main.transform.position + Camera.main.transform.forward * 2f;
+        transform.Rotate(transform.forward, Random.Range(-m_throwRotationRandom, m_throwRotationRandom));
+        var lookRotation = Quaternion.LookRotation(Camera.main.transform.forward, transform.up);
+        transform.rotation = lookRotation;
+        
+        m_rigidBody.AddForce(Camera.main.transform.forward * 20f, ForceMode.Impulse);
+        m_trail.gameObject.SetActive(true);
+    }
+
+    public void ReturnAxe(Action onAxeReturnedCallback)
+    {
+        m_isReturning = true;
+        m_flying = false;
+        m_rigidBody.isKinematic = false;
+        m_rigidBody.velocity = Vector3.zero;
+        m_currentReturningTime = 0f;
+        var lookRotation = Quaternion.LookRotation(Camera.main.transform.forward, transform.up);
+        transform.rotation = lookRotation;
+        m_landPosition = transform.position;
+        m_onAxeReturnedCallback = onAxeReturnedCallback;
+    }
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        m_flying = false;
+        m_rigidBody.isKinematic = true;
+    }
+
+    Vector3 GetBezierCurve(float t, Vector3 p0, Vector3 p1, Vector3 p2)
+    {
+        float u = 1 - t;
+        float tt = t * t;
+        float uu = u * u;
+
+        Vector3 p = (uu * p0) + (2 * u * t * p1) + (tt * p2);
+        return p;
+    }
+}
---
+++ Experimenting/Assets/Scripts/AxeThrowing/CharacterController.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterController : MonoBehaviour
+{
+    // since we dont have proper animation blending yet everything is done through states
+    public enum PlayerState
+    {
+        Idle,
+        Walking,
+        Aiming,
+        Throwing,
+        CallingAxe,
+    }
+    
+    [SerializeField] private Axe m_axe;
+    [SerializeField] private Animator m_animator;
+    [SerializeField] private float m_maxMoveSpeed = 4;
+    [SerializeField] private float m_acceleration = 4;
+    [SerializeField] private float m_yawRotationSpeed = 90;
+    [SerializeField] private Image m_crossHair;
+
+    private PlayerState m_currentState;
+
+    private int WALKING_BOOL = Animator.StringToHash("walking");
+    private int FORWARD_MULTIPLIER = Animator.StringToHash("forwardMultiplier");
+    private int AIMING_BOOL = Animator.StringToHash("aiming");
+    private int CALLING_AXE_BOOL = Animator.StringToHash("callingAxe");
+    private int THROW_TRIGGER = Animator.StringToHash("throw");
+
+    private bool m_axeThrown = false;
+
+    private bool m_cancelledThrow;
+    
+    private float m_currentSpeed;
+    private Vector3 m_movementVector;
+
+    private void Awake()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    
+    private void Update()
+    {
+        switch (m_currentState)
+        {
+            case PlayerState.Idle:
+                if (!m_axeThrown && CheckForAimInput())
+                {
+                    SwitchState(PlayerState.Aiming);
+                }
+                else if (m_axeThrown && CheckForCallAxeInput())
+                {
+                    SwitchState(PlayerState.CallingAxe);
+                }
+                else if (CheckForMovementInput())
+                {
+                    SwitchState(PlayerState.Walking);
+                }
+
+               
+                break;
+            case PlayerState.Walking:
+                if (!m_axeThrown && CheckForAimInput())
+                {
+                    SwitchState(PlayerState.Aiming);
+                }
+                else if (m_axeThrown && CheckForCallAxeInput())
+                {
+                    SwitchState(PlayerState.CallingAxe);
+                }
+                else if (!CheckForMovementInput())
+                {
+                    SwitchState(PlayerState.Idle);
+                }
+                break;
+            case PlayerState.Aiming:
+                if (CheckForAimInputRelease())
+                {
+                    if (m_cancelledThrow)
+                    {
+                        SwitchState(PlayerState.Idle);
+                    }
+                    else
+                    {
+                        SwitchState(PlayerState.Throwing);
+                    }
+                }
+                break;
+            case PlayerState.Throwing:
+                break;
+            case PlayerState.CallingAxe:
+                break;
+        }
+        
+        float yawRotation = Input.GetAxis("Mouse X");
+        transform.Rotate(transform.up, yawRotation * m_yawRotationSpeed * Time.deltaTime);
+    }
+
+    private bool CheckForAimInput()
+    {
+        bool hasInput = Input.GetMouseButtonDown(0);
+        return hasInput;
+    }
+    
+    private bool CheckForCallAxeInput()
+    {
+        bool hasInput = Input.GetMouseButtonDown(1);
+        return hasInput;
+    }
+
+
+    // should only be called in aim state
+    private bool CheckForAimInputRelease()
+    {
+        bool hasInput = false;
+        if (Input.GetMouseButtonDown(1))
+        {
+            hasInput = true;
+            m_cancelledThrow = true;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            hasInput = true;
+            m_cancelledThrow = false;
+        }
+
+        return hasInput;
+    }
+    
+    private void SwitchState(PlayerState newState)
+    {
+        Debug.Log("New State: " + newState);
+        m_currentState = newState;
+
+        switch (newState)
+        {
+            case PlayerState.Idle:
+                m_animator.SetBool(WALKING_BOOL, false);
+                m_animator.SetBool(AIMING_BOOL, false);
+                m_animator.SetBool(CALLING_AXE_BOOL, false);
+                m_crossHair.gameObject.SetActive(false);
+                break;
+            case PlayerState.Walking:
+                m_currentSpeed = 0;
+                m_animator.SetBool(WALKING_BOOL, true);
+                break;
+            case PlayerState.Aiming:
+                m_animator.SetBool(WALKING_BOOL, false);
+                m_animator.SetBool(AIMING_BOOL, true);
+                m_crossHair.gameObject.SetActive(true);
+                break;
+            case PlayerState.Throwing:
+                m_axeThrown = true;
+                m_animator.SetTrigger(THROW_TRIGGER);
+                m_crossHair.gameObject.SetActive(false);
+               // m_animator.SetBool(AIMING_BOOL, false);
+                break;
+            case PlayerState.CallingAxe:
+                m_animator.SetBool(CALLING_AXE_BOOL, true);
+                m_axe.ReturnAxe(OnAxeReturned);
+                break;
+        }
+    }
+    
+    public void ThrowAnimationComplete()
+    {
+        Debug.Log("Throw down");
+        SwitchState(PlayerState.Idle);
+    }
+
+    private void OnAxeReturned()
+    {
+        Debug.Log("Axe returned");
+        m_axeThrown = false;
+        SwitchState(PlayerState.Idle);
+    }
+    
+    private bool CheckForMovementInput()
+    {
+        bool hasInput = false;
+        float sideMovement = Input.GetAxis("Horizontal");
+        float straightMovement = Input.GetAxis("Vertical");
+
+
+        if (sideMovement != 0f || straightMovement != 0f)
+        {
+            m_movementVector = transform.forward * straightMovement + transform.right * sideMovement;
+            m_movementVector.Normalize();
+
+            m_currentSpeed += m_acceleration * Time.deltaTime;
+            if (m_currentSpeed > m_maxMoveSpeed)
+            {
+                m_currentSpeed = m_maxMoveSpeed;
+            }
+            
+            m_animator.SetFloat(FORWARD_MULTIPLIER, straightMovement > 0 ? 1f : -1f);
+
+            // TODO: velocity instead
+            transform.position += m_movementVector * m_currentSpeed * Time.deltaTime;
+            hasInput = true;
+
+        } 
+        else
+        {
+            hasInput = false;
+        }
+
+        return hasInput;
+    }
+
+
+    public void ThrowAxe()
+    {
+        m_axe.FlyForward();
+    }
+}
